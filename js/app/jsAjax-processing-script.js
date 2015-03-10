b53cc3e043b0b86bab7b59fec9f16593f1592cb3
Patrick <pat2echo@gmail.com>
2015-03-10 17:14:41 +0100

    1.1.10

--- js/app/jsAjax-processing-script.js
+++ js/app/jsAjax-processing-script.js
@@ -167,6 +167,7 @@ function cannot_initiate_app(){
             message_message: 'Please try signing in again. \nIf problem persists please contact our customer care',
             auto_close: 'no'
         };
+        clearData();
     }else{
         var settings = {
             message_title:'Invalid Device ID',
@@ -174,7 +175,6 @@ function cannot_initiate_app(){
             auto_close: 'no'
         };
     }
-	clearData();
 	display_popup_notice( settings );
     $.mobile.navigate( "#signup", { transition : "none" });
 };
